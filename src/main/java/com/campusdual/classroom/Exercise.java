ca01071e548b192a95e40013245ce9ba5ac84a04
Mario Rodríguez Martínez <mario.rodriguez.martinez.lu@gmail.com>
2025-11-19 11:32:58 +0100

    Solved exercise 30

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,7 +1,21 @@
 package com.campusdual.classroom;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exercise {
     public static void main(String[] args) {
+        try(BufferedReader br = new BufferedReader(new FileReader("src/main/resources/lorem.txt"))){
+            String line;
+            while( (line = br.readLine()) !=null){
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
